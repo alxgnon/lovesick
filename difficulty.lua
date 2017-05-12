372675e394b6ddb8e980a196a4469055d35ecc25
Alexandre Gagnon <alxgnon@gmail.com>
2017-05-12 17:09:47 -0400

    better difficulty scaling

--- difficulty.lua
+++ difficulty.lua
@@ -1,63 +1,45 @@
 difficulty = {}
 
 local levels = {
-  {pawnSpeed = 2.000, pawnRate = 0.600},
-  {pawnSpeed = 2.300, pawnRate = 0.570},
-  {pawnSpeed = 2.700, pawnRate = 0.530},
-  {pawnSpeed = 3.000, pawnRate = 0.500},
-  {pawnSpeed = 3.200, pawnRate = 0.480},
-  {pawnSpeed = 3.400, pawnRate = 0.460},
+  {pawnSpeed = 0.4, pawnRate = 0.1},
+  {},
+  {pawnSpeed = 0.2, pawnRate = 0.05},
+  {},
+  {pawnSpeed = 0.1, pawnRate = 0.02},
+  {},
 
-  {pawnSpeed = 3.600, pawnRate = 0.440},
-  {pawnSpeed = 3.800, pawnRate = 0.420},
-  {pawnSpeed = 4.000, pawnRate = 0.400},
-  {pawnSpeed = 4.050, pawnRate = 0.390},
-  {pawnSpeed = 4.100, pawnRate = 0.380},
-  {pawnSpeed = 4.150, pawnRate = 0.370},
-
-  {pawnSpeed = 4.200, pawnRate = 0.365},
-  {pawnSpeed = 4.250, pawnRate = 0.360},
-  {pawnSpeed = 4.300, pawnRate = 0.355},
-  {pawnSpeed = 4.310, pawnRate = 0.350},
-  {pawnSpeed = 4.320, pawnRate = 0.345},
-  {pawnSpeed = 4.330, pawnRate = 0.340},
-
-  {pawnSpeed = 4.340, pawnRate = 0.335},
-  {pawnSpeed = 4.350, pawnRate = 0.330},
-  {pawnSpeed = 4.355, pawnRate = 0.325},
-  {pawnSpeed = 4.360, pawnRate = 0.320},
-  {pawnSpeed = 4.365, pawnRate = 0.315},
-  {pawnSpeed = 4.370, pawnRate = 0.310},
-
-  {pawnSpeed = 4.475, pawnRate = 0.305},
-  {pawnSpeed = 4.380, pawnRate = 0.300},
-  {pawnSpeed = 4.385, pawnRate = 0.295},
-  {pawnSpeed = 4.390, pawnRate = 0.290},
-  {pawnSpeed = 4.395, pawnRate = 0.285},
-  {pawnSpeed = 4.400, pawnRate = 0.280}
+  {},
+  {},
+  {pawnSpeed = 0.025, pawnRate = 0.005}
 }
 
 local score = 0
-local lastLevel = 0
 
 function difficulty.reset()
   score = 0
   lastLevel = -1
-  difficulty.update(0)
 end
 
 function difficulty.update(s)
   score = s
   local level = math.floor(score / 10)
-  
-  if level > lastLevel then
-    lastLevel = level
-    local lv = levels[level + 1]
-    if not lv then
-      lv = levels[table.getn(levels)]
-    end
 
-    difficulty.pawnSpeed = lv.pawnSpeed or difficulty.pawnSpeed
-    difficulty.pawnRate = lv.pawnRate or difficulty.pawnRate
+  difficulty.pawnSpeed = 2.4
+  difficulty.pawnRate = 0.6
+
+  local pawnSpeed = 0
+  local pawnRate = 0
+
+  for i = 0, level do
+    local lv = levels[i]
+    if i > 0 then
+      if lv then
+        pawnSpeed = lv.pawnSpeed or pawnSpeed
+        pawnRate = lv.pawnRate or pawnRate
+      end
+
+      difficulty.pawnSpeed = difficulty.pawnSpeed + pawnSpeed
+      difficulty.pawnRate = difficulty.pawnRate - pawnRate
+    end
   end
 end
